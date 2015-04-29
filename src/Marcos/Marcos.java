852ff119ea51c0dd34cb2edb80630006fed9aaa8
Diurno <Diurno@C7PC19>
2015-04-29 08:55:00 +0200

    Primer commit del proyecto, contiene un "Hola Mundo"

---
+++ src/Marcos/Marcos.java
@@ -0,0 +1,13 @@
+
+package Marcos;
+
+/**
+ *Esta clase contiene un "Hola Mundo"
+ * @author Marcos
+ */
+public class Marcos {
+
+   public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+}
