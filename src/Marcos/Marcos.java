f9ac1bd3ceccada529cb2da8e05c53194fe20b02
Diurno <Diurno@C7PC19>
2015-04-29 08:58:52 +0200

    Segundo commit realizado, este se hace de forma directa. Se ha añadido un comentario en la implementación del Hola Mundo.

--- src/Marcos/Marcos.java
+++ src/Marcos/Marcos.java
@@ -8,6 +8,6 @@ package Marcos;
 public class Marcos {
 
    public static void main(String[] args) {
-    System.out.println("Hola mundo");
+    System.out.println("Hola mundo"); // Esto es un hola mundo
   }
 }
