5a54d88c937c761294e93e63c6cf89694f2869c9
Diurno <Diurno@C7PC19>
2015-05-06 08:51:50 +0200

    Tercer commit realizado, se hace de forma directa. Se ha añadido la clase Resta

---
+++ src/Marcos/Resta.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Marcos;
+
+/**
+ * Esta clase resta dos números, uno mayor y otro menor
+ * 
+ * @version 1.0
+ * @author Marcos
+ */
+public class Resta {
+    
+    public Resta ()
+    {
+        
+    }
+    
+    /**
+     * 
+     * @param numeroMayor el número mayor al que le restaremos el otro
+     * @param numeroMenor el número que se resta
+     * @return int el resultado de la resta
+     */
+    public int restar(int numeroMayor, int numeroMenor)
+    {
+        return numeroMayor - numeroMenor;
+    }
+}
